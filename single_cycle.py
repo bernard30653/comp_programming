e478d7f8ee8cc7c6ce66666a82db049f4e29cd90
Bernard <bernard30653@gmail.com>
2021-01-03 18:36:17 +0800

    Finished implementing Single Cycle Check

---
+++ single_cycle.py
@@ -0,0 +1,19 @@
+# This is a question from algoexpert Single Cycle Check
+# Given an array where each element represent the number of jumps(i.e. 2 means 2 indices forward
+# and -4 means 4 indices backward
+# Check if an input array is such that there is a single cycle
+def hasSingleCycle(array):
+    # Write your code here.
+    arrLen = len(array)
+    elemVisited = 0
+	currIdx = 0
+	while elemVisited < arrLen:
+		if array[currIdx] ==0:  #e.g. [1,1,0]
+			return False
+		if elemVisited > 0 and currIdx == 0:
+			return False
+		currIdx = (currIdx+array[currIdx])%arrLen
+		elemVisited +=1
+	if currIdx !=0:
+		return False
+	return True
